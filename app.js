07e4ad4fbbeecb82207addd2fbb9fb4fdfb7eb76
NenoyHeker <106822874+NenoyHeker@users.noreply.github.com>
2023-10-23 22:47:39 -0600

    Copia del codigo

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+const port = 7070
+
+
+const amigoRoutes=require('./amigos/routes/amigo.routes')
+
+app.use('/amigos',amigoRoutes)
+
+app.listen(port,()=>console.log(`Servidor escuchando en http://localhost${port}`))
